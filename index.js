327c1fb94c20e9a21d0286115f66f20734b2978c
Mohammad Naim Faizy <mnaim.faizy@gmail.com>
2023-08-12 20:44:45 +0500

    chore: fixed the endpoints to work

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose');
+const multer = require('multer');
+const upload = multer();
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
@@ -16,6 +18,14 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+// Define User Schema
+const userSchema = new mongoose.Schema({
+  username: String
+});
+
+const User = mongoose.model('User', userSchema);
+
+
 // Define Exercise Schema
 const exerciseSchema = new mongoose.Schema({
   username: String,
@@ -28,6 +38,20 @@ const Exercise = mongoose.model('Exercise', exerciseSchema);
 
 // Middleware
 app.use(express.json());
+app.use(upload.none()); // Parse FormData
+
+// Route to add a new user
+app.post('/api/users', async (req, res) => {
+  const { username } = req.body;
+  try {
+    const user = new User({ username });
+    await user.save();
+    res.json(user);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: 'Server error' });
+  }
+});
 
 // Route to add a new exercise
 app.post('/api/exercise/add', async (req, res) => {
@@ -56,6 +80,45 @@ app.get('/api/exercise/all', async (req, res) => {
   }
 });
 
+
+// Route to get user's exercise log
+app.get('/api/users/:_id/logs', async (req, res) => {
+  const userId = req.params._id;
+  const { from, to, limit } = req.query;
+
+  try {
+    const userExercises = await Exercise.find({ username: userId })
+      .where('date').gte(from || '1970-01-01').lte(to || '2100-01-01')
+      .limit(Number(limit) || 0);
+
+    res.json(userExercises);
+  } catch (error) {
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
+// Route to add user's exercise
+app.post('/api/users/:_id/exercises', async (req, res) => {
+  const userId = req.params._id;
+  const { description, duration, date } = req.body;
+
+  if (!description || !duration) {
+    return res.json({ error: 'Description and duration are required' });
+  }
+
+  try {
+    const exercise = new Exercise({
+      username: userId,
+      description,
+      duration,
+      date: date || new Date()
+    });
+    await exercise.save();
+    res.json(exercise);
+  } catch (error) {
+    res.status(500).json({ error: 'Server error' });
+  }
+});
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
